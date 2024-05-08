2b9d381236e1231e651aa3ff168789f4e9ce7981
Bence Molnár <bence.molnar@snapsoft.hu>
2024-05-08 21:52:36 +0200

    reformat & add nav items

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import '@fontsource-variable/archivo/wdth.css';
+  import '../app.css';
+</script>
+
+<header>
+  <section class="gutter"></section>
+  <section class="m-24">
+    <h1>Bence Molnár</h1>
+    <p>Web Developer</p>
+  </section>
+</header>
+<slot></slot>
+
+<style>
+  header {
+    border-bottom: solid var(--border-width) var(--border-color);
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    text-transform: uppercase;
+  }
+
+  .gutter {
+    height: 7rem;
+    width: min(20rem, 100vw);
+    background: #000;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+  <nav>
+    <ul>
+      <li>
+        <a href="/work">
+          <h2 class="nav-item">Work</h2>
+        </a>
+      </li>
+      <li>
+        <a href="/inspiration">
+          <h2 class="nav-item">Inspiration</h2>
+        </a>
+      </li>
+      <li></li>
+      <li>
+        <a href="/contact">
+          <h2 class="nav-item">Contact</h2>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</main>
+
+<style>
+  nav ul {
+    display: grid;
+    grid-template-columns: 20rem 20rem auto 20rem;
+    border-bottom: solid var(--border-width) var(--border-color);
+    text-transform: uppercase;
+    text-align: right;
+  }
+
+  li {
+    height: 23rem;
+  }
+
+  li:not(:last-child) {
+    border-right: solid var(--border-width) var(--border-color);
+  }
+
+  li:last-child {
+    grid-column: 4;
+  }
+
+  h2 {
+    margin: 1rem;
+  }
+</style>
