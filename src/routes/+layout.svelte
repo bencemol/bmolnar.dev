23af4e986ddf5b5e5dea24f7d54f73a650b9f115
Bence Molnár <bence.molnar@accretivetg.com>
2024-05-10 11:02:08 +0200

    preload font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
   import '@fontsource-variable/archivo/wdth.css';
+  import archivoWoff from '@fontsource-variable/archivo/files/archivo-latin-wdth-normal.woff2?url';
   import '../app.css';
   import Header from '$lib/Header.svelte';
 </script>
+
+<svelte:head>
+  <link rel="preload" as="font" type="font/woff2" href={archivoWoff} crossorigin="anonymous" />
+</svelte:head>
 
 <Header />
 <slot></slot>
